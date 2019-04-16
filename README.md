5d39e4099daa820f945a64f4f47cb7f4284615c6
Yansy <yansiyu.old.dog@hotmail.com>
2019-04-17 00:54:55 +0800

    update

---
+++ README.md
@@ -0,0 +1,13 @@
+## start
+**database with docker** 
+```bash
+docker run --name some-postgres -e POSTGRES_PASSWORD=mysecretpassword -d postgres
+```
+```bash
+docker run --name EngCalc-local-postgres -e POSTGRES_PASSWORD=FeintVeela-Vagrant -d postgress
+```
+
+**need Download nltk recource**
+```bash
+python install_nltk_resource
+```
--- manage.py
+++ manage.py
@@ -1,11 +1,15 @@
-from flask import Flask
-from flask import Flask
+import os
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
-def factory():
-    app = Flask(__name__)
-    return app
+from app import app, db
 
+app.config.from_object(os.environ['APP_SETTINGS'])
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
-    app = factory()
-    app.run()
+    manager.run()
